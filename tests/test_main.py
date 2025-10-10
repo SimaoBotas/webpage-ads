dd1eea5e5ee036c72564e7444dea8ef46aeb93f1
Simão Botas <botassimao@gmail.com>
2025-10-10 15:22:03 +0100

    teste

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,13 @@
-from src import main 
+# tests/test_main.py
+import sys
+import os
+
+# Adiciona a raiz do projeto ao caminho de importação
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src import main  # agora deve funcionar
 
 def test_hello_world(capsys):
-    
-    main  
-    captured = capsys.readouterr()
+    main  # executa o print do main.py
+    captured = capsys.readouterr()  # captura a saída do print
     assert "Hello world" in captured.out
